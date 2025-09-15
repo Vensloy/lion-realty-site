092be623c5034a11e5b3bf2909cd4ab6973a5e89
envoker <komogorcevanton10@gmail.con>
2025-09-15 20:17:06 +0800

    Maked the header section

--- src/assets/pages/desktop/components/Header.jsx
+++ src/assets/pages/desktop/components/Header.jsx
@@ -2,10 +2,29 @@
 // -- Стили
 import '../css/header.css'
 
+// -- Шрифты
+import '../css/fonts/circe.css'
+
 export default function Header() {
     return (
         <header>
-            <div className='fon-color'></div>
+            <div className='fon-color'>
+                <section className='header'>
+                    <menu>
+                        <img src="/img/pages/desktop/header/header/menu.svg" alt="" />
+                        <p>Меню</p>
+                    </menu>
+
+                    <img src="/img/pages/desktop/header/header/logo.svg" alt="" />
+
+                    <nav>
+                        <img src="/img/pages/desktop/header/header/messengers/viber.svg" alt="" />
+                        <img src="/img/pages/desktop/header/header/messengers/whatsapp.svg" alt="" />
+                        <img src="/img/pages/desktop/header/header/messengers/telegram.svg" alt="" />
+                        <p>+7 (978) 888-1-654</p>
+                    </nav>
+                </section>
+            </div>
         </header>
     )
 }
